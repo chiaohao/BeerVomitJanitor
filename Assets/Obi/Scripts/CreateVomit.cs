84c372fb6e4849bd9fc7c7710e780a20ad081aad
chiaohao <ksjoey0096@gmail.com>
2017-12-16 17:14:23 +0800

    import assets

---
+++ Assets/Obi/Scripts/CreateVomit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Obi;
+
+public class CreateVomit : MonoBehaviour {
+
+	public GameObject Camera;
+	public GameObject Vomit;
+	private GameObject V;
+	// Use this for initialization
+	void Start () {
+		V = Instantiate(Vomit, new Vector3(0f, 4f, 0f), Quaternion.Euler(-45, 90, 0));
+		Debug.Log (Camera.GetComponents<ObiFluidRenderer> ()[0].particleRenderers[1] = V.transform.GetChild (0).GetComponent<ObiParticleRenderer> ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F)){
+			V.transform.GetChild (0).GetComponent<ObiEmitter> ().speed = 4;
+		}
+	}
+}
