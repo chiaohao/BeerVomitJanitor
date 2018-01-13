1844fd56e82e9dc7ffd6b12d2e4289aa51c27dbe
hikariNTU <hikari@ntu.im>
2018-01-14 02:35:06 +0800

    control over pp profile instance

--- Assets/scripts/Debug/CtrlableCamera.cs
+++ Assets/scripts/Debug/CtrlableCamera.cs
@@ -3,29 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CtrlableCamera : MonoBehaviour {
-	
+
+	[SerializeField]
+	private PPManager ppp;
+	[SerializeField]
+	private float alcoholValue = 0f;
 	public float sensitivityX = 15F;
-	public float runSpeed = 10,
+	public float runSpeed = 3,
 	sprintSpeed = 20;
 
 	// Use this for initialization
 	private Transform cameraTransform;
 	void Start () {
 		cameraTransform = Camera.main.transform;
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey("w")) {
-			if (Input.GetKey (KeyCode.LeftShift)) {
-				transform.position += cameraTransform .forward * (Time.deltaTime * sprintSpeed);
-				Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 80, 2*Time.deltaTime);
-			}
-			else {
-				transform.position += cameraTransform .forward * (Time.deltaTime * runSpeed);
-				Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 60, 2*Time.deltaTime);
-			}
+			transform.position += Vector3.forward * (Time.deltaTime * runSpeed);
 		}
 		if (Input.GetKey("s")) {
 			transform.position += Vector3.back * (Time.deltaTime * runSpeed);
@@ -35,6 +31,23 @@ public class CtrlableCamera : MonoBehaviour {
 		}
 		if (Input.GetKey("d")) {
 			transform.position += Vector3.right * (Time.deltaTime * runSpeed);
+		}
+		if (Input.GetKey("q")) {
+			transform.position += Vector3.down * (Time.deltaTime * runSpeed);
+		}
+		if (Input.GetKey("e")) {
+			transform.position += Vector3.up * (Time.deltaTime * runSpeed);
+		}
+		if (Input.GetKeyDown ("p")) {
+			ppp.toggleHeavyEffect ();
+		}
+		if (Input.GetKeyDown ("9")) {
+			alcoholValue = Mathf.Clamp01 (alcoholValue - 0.1f);
+			ppp.SetAlcoholValue (alcoholValue);
+		}
+		if (Input.GetKeyDown ("0")) {
+			alcoholValue = Mathf.Clamp01 (alcoholValue + 0.1f);
+			ppp.SetAlcoholValue (alcoholValue);
 		}
 	}
 }
---
+++ Assets/scripts/PPManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+[RequireComponent(typeof(PostProcessingBehaviour))]
+
+public class PPManager : MonoBehaviour {
+	[SerializeField]
+	private PostProcessingProfile m_Profile;
+	[SerializeField]
+	private bool heavyEffect = true;
+
+	void OnEnable()
+	{
+		var behaviour = GetComponent<PostProcessingBehaviour>();
+
+		if (behaviour.profile == null)
+		{
+			enabled = false;
+			return;
+		}
+
+		m_Profile = Instantiate(behaviour.profile);
+		behaviour.profile = m_Profile;
+		m_Profile.motionBlur.enabled = true;
+	}
+
+	void Update()
+	{
+//		var vignette = m_Profile.vignette.settings;
+//		vignette.smoothness = Mathf.Abs(Mathf.Sin(Time.realtimeSinceStartup) * 0.99f) + 0.01f;
+//		m_Profile.vignette.settings = vignette;
+	}
+
+	public bool SetAlcoholValue(float value){
+		value = Mathf.Clamp01(Mathf.Abs (value));
+
+		var motion = m_Profile.motionBlur.settings;
+		motion.shutterAngle = value;
+		motion.frameBlending = value;
+		m_Profile.motionBlur.settings = motion;
+
+		if (m_Profile.chromaticAberration.enabled == false)
+			m_Profile.chromaticAberration.enabled = true;
+		var aberration = m_Profile.chromaticAberration.settings;
+		aberration.intensity = value;
+		m_Profile.chromaticAberration.settings = aberration;
+
+		if (m_Profile.vignette.enabled == false)
+			m_Profile.vignette.enabled = true;
+		var vignette = m_Profile.vignette.settings;
+		vignette.smoothness = Mathf.Clamp01 (value + 0.01f);
+		m_Profile.vignette.settings = vignette;
+
+		return true;
+	}
+
+	public bool toggleHeavyEffect(){
+		m_Profile.ambientOcclusion.enabled = !heavyEffect;
+		m_Profile.antialiasing.enabled = !heavyEffect;
+		m_Profile.bloom.enabled = !heavyEffect;
+		heavyEffect = !heavyEffect;
+		return heavyEffect;
+	}
+}
